c9b0ace7da721279966b06b9af65811b07e0024d
7chii <marianapacper@outlook.com>
2024-06-23 20:20:16 -0300

    pre entr

--- server.js
+++ server.js
@@ -1,26 +1,71 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const sql = require('mysql');
 const port = 8080;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const {MongoClient} = require('mongodb');
+const { ALL } = require('dns');
 //conn bd (arrumar pra maquina externa)
-
-/*const bd = sql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "chess"
-});
-
-//aumentar limite de json recebido/mandado(para passar base64)
-app.use(express.json({ limit: '50mb' }));
-*/
-
+const url = 'mongodb://localhost:27017/';
+const client = new MongoClient(url);
+async function query(){
+try {
+    const conn = await client.connect();
+   const db = await conn.db('chess');
+   const coll = await db.collection('logInfo');
+   const result = await coll.find().toArray();
+   console.log(result);
+   conn.close();
+   return result;
+} catch (err){(console.log(err))};
+return;
+}
+async function login(obj, ojb){
+    try {const conn = await client.connect();
+        const db = await conn.db('chess');
+        const coll = await db.collection('logInfo');
+        const result = await coll.find({"user":'${obj}',"passw":'${ojb}'}).toArray();
+        conn.close();
+        if(result !== undefined){
+            return 'true'
+        }else{return 'false'}
+    } catch (err){(console.log(err))};
+    return;
+}
 //css include
 app.use('/static', express.static('static'));
 //routes
 app.get('/', (req, res)=>{
-    res.render('index.ejs');
+    res.render('index.ejs',
+        {log:'false'}
+    );
+})
+//retirar
+app.get('/query', async (req, res)=>{
+    const result = await query();
+    res.send(result);
+})
+app.post('/LOG', async(req, res)=>{
+   // let obj = req.body.username;
+    //let ojb = req.body.password;
+    try {
+        const {user} = req.body;
+    const {passw} = req.body;
+    const {authorization} = req.headers;
+    const result = await login(user, passw);
+    if(result === 'true'){
+        console.log('true');
+        return res.render('index.ejs',{log:'true'})
+    }else{
+        console.log("nan")
+        return res.render('index.ejs',{log:'nan'})
+    }
+    } catch (err){(console.log(err))};
+    return; 
 })
 app.get('/logsign/views/style.css', (req, res)=>
 {
